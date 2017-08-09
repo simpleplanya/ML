52078c317abfbc5d4775ff8ad008730da3a880df
Rocky <498g0102@stust.edu.tw>
2017-08-09 16:49:58 +0800

    Add files via upload

---
+++ clean_data.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 01 20:33:08 2017
+
+@author: Rocky
+"""
+
+import re
+import pandas as pd
+from nltk.stem.porter import PorterStemmer
+
+def preprocessor(text):
+    text = re.sub('<[^>]*>', '', text)
+    emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)', text)
+    text = re.sub('[\W]+', ' ', text.lower()) + \
+           ' '.join(emoticons).replace('-', '')
+    return text
+
+
+
+
+def tokenizer(text):
+    return text.split()
+def tokenizer_porter(text):
+    porter = PorterStemmer()
+    return [porter.stem(word) for word in text.split()]
+
+# test preprocessor function  
+#print preprocessor("</a>This :) is :( a test :-)!")
+    
+   
+# read csv data 
+df = pd.read_csv('movie_data.csv')
+df['review'] = df['review'].apply(preprocessor)
+print 'clean ok '
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop = stopwords.words('english')
+[w for w in tokenizer_porter('a runner likes running and runs a lot')[-10:] if w not in stop]
+
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
+               
+    
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+tfidf = TfidfVectorizer(strip_accents=None, 
+                        lowercase=False, 
+                        preprocessor=None)
+
+param_grid = [{'vect__ngram_range': [(1,1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+             {'vect__ngram_range': [(1,1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'vect__use_idf':[False],
+               'vect__norm':[None],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+             ]
+
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression(random_state=0))])
+
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid, 
+                           scoring='accuracy',
+                           cv=5, verbose=1,
+                           n_jobs=-1)               
+               
+
+
+gs_lr_tfidf.fit(X_train, y_train)
+
+print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
+print('Best parameter set: %s ' % gs_lr_tfidf.best_params_)
+print('CV Accuracy: %.3f' % gs_lr_tfidf.best_score_)
